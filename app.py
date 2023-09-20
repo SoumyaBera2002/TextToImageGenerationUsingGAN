e2ea3dd3ae6a5f1a8eca8cda0ef5e9bba4e3fec2
Soumya Bera <berasoumya2002@gmail.com>
2023-09-20 21:11:42 +0530

    Big Changes

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask,render_template, redirect, url_for,request
 
-app = Flask(__name__)
+app = Flask(__name__,static_url_path='/static')
 
 @app.route("/")
-def hello_world():
+def base():
+    return render_template('index.html')
+
+@app.route("/home")
+def home():
     return render_template('index.html')
 
 @app.route("/generate",methods=["GET","POST"])
